a1201dd6c5347da510b7dab7841ade86860c6e7a
qweqwe1qq <qweqwe@gmail.com>
2025-08-21 20:55:38 +0400

    commit

---
+++ day32/classwork/cw2.py
@@ -0,0 +1,5 @@
+# 2)შექენით სეტები და განიხილეთ ოპერატორები (union,intersection,difference,symmetric_difference,clear,add,remove) 
+# ახსენით ასევე კომენტარებით და გააკეთეთ ორივე ხერხით როგორც ტექსტიტ ისე სიმბოლოთი.
+
+
+
